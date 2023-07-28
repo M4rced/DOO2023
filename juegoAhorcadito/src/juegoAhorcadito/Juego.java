c71090fb0ad0fcd19a8312ef4baddcb50a6f343b
Jorge Andres Carmona Ramirez <jorgeandresR03@gmail.com>
2023-07-28 17:16:23 -0500

    Clases completas

--- juegoAhorcadito/src/juegoAhorcadito/Juego.java
+++ juegoAhorcadito/src/juegoAhorcadito/Juego.java
@@ -1,5 +1,26 @@
-package juegoAhorcadito;
+public class JuegoAhorcadoApp {
+    public static void main(String[] args) {
+        System.out.println("¡Comienza el juego de ahorcado!");
+        Scanner scanner = new Scanner(System.in);
 
-public class Juego {
+        System.out.print("Ingresa tu nombre: ");
+        String nombreJugador = scanner.nextLine();
 
+        System.out.print("Ingresa la palabra oculta para el juego: ");
+        String palabraOculta = scanner.nextLine();
+
+        JuegoAhorcado juego = new JuegoAhorcado(palabraOculta);
+        Jugador jugador = new Jugador(nombreJugador);
+
+        while (true) {
+            System.out.println("Palabra actual: " + juego.mostrarPalabra());
+            char letra = jugador.adivinarLetra();
+            String resultado = juego.intentarLetra(letra);
+            System.out.println(resultado);
+
+            if (!new String(juego.mostrarPalabra()).contains("_") || juego.intentosRealizados >= juego.intentosMaximos) {
+                break;
+            }
+        }
+    }
 }
--- juegoAhorcadito/src/juegoAhorcadito/Jugador.java
+++ juegoAhorcadito/src/juegoAhorcadito/Jugador.java
@@ -1,41 +1,15 @@
-package juegoAhorcadito;
+import java.util.Scanner;
 
 public class Jugador {
-	private String nombre;
-	private int puntaje;
-	private int totalPartidasJugadas;
-	private int totalPartidasGanadas;
-	private int totalPartidasPerdidas;
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public int getPuntaje() {
-		return puntaje;
-	}
-	public void setPuntaje(int puntaje) {
-		this.puntaje = puntaje;
-	}
-	public int getTotalPartidasJugadas() {
-		return totalPartidasJugadas;
-	}
-	public void setTotalPartidasJugadas(int totalPartidasJugadas) {
-		this.totalPartidasJugadas = totalPartidasJugadas;
-	}
-	public int getTotalPartidasGanadas() {
-		return totalPartidasGanadas;
-	}
-	public void setTotalPartidasGanadas(int totalPartidasGanadas) {
-		this.totalPartidasGanadas = totalPartidasGanadas;
-	}
-	public int getTotalPartidasPerdidas() {
-		return totalPartidasPerdidas;
-	}
-	public void setTotalPartidasPerdidas(int totalPartidasPerdidas) {
-		this.totalPartidasPerdidas = totalPartidasPerdidas;
-	}
-	
-	
+    private String nombre;
+
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public char adivinarLetra() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(nombre + ", ingresa una letra: ");
+        return scanner.next().toLowerCase().charAt(0);
+    }
 }
